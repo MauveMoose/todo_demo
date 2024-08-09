48717eefaede383880d052928e0dba1b5df6c581
admin <admin@31D-LAB5-05.SVV.local>
2024-08-09 16:40:24 +0530

    Upload 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Input from "./components/Input";
-
-
+import {useState} from "react";
 //A map is like a for loop, that performs the contained callback function on each of the iterated elements.
 //Unlike a for loop, it also stores each of the results in an array.
 function learnMap() {
@@ -12,16 +11,38 @@ function learnMap() {
 
 //A map is used here to iterate through each of the elements and create an input field from all of them
 function App() {
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  function handleSubmit(){
+    console.log(title + " " + desc);
+    setTitle("");
+    setDesc("");
+  }
+  
+  function handleChange(event){
+    if(event.target.name==="Title"){
+    setTitle(event.target.value);}
+    else if(event.target.name==="Description"){
+    setDesc(event.target.value);}
+
+  }
+
   //let arr = ["Title: ", "Desc: "]
   //let elements = arr.map((i) => <Input label= {i}></Input>);
   return (
     <div>
       <ul>
-      <Input label = {"Title: "}></Input>
-      <Input label = {"Description: "}></Input>
+      <li> <Input label = {"Title"} value={title} handleChange= {handleChange}></Input> </li>
+      <li> <Input label = {"Description"} value={desc} handleChange= {handleChange}></Input></li>
+      <li> <button onClick = {handleSubmit}> Click Me </button></li>
       </ul>
     </div>
   );
 }
 
 export default App;
+//git branch
+//git switch master
+//git pull
+//git switch back to your original branch
+//git merge master
--- frontend/src/components/Input.js
+++ frontend/src/components/Input.js
@@ -1,12 +1,18 @@
+//useState reprints the page each time a change is made
+
+
 //Function to make an input field. 
 //It takes "props" as an input when called. 
 //It makes a label out of the value called "props.label" that was input
 //The text input field will have placeholder text. This placeholder will also be input when the funciton is called.
-function Input(props) {
+function Input({label, value, handleChange}) {
+  
+  //Function that handles every 
+
     return(
       <div>
-        <label>Enter {props.label}</label>
-        <input placeholder={`Enter ${props.label}`} type="text"></input>
+        <label>Enter {label}</label>
+        <input type="text" onChange={handleChange} name= {label} value={value}></input><br></br>
       </div>
     );
   }
